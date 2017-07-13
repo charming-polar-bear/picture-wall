988a6a1a3f090ccd262e8f08656785c538e3d48e
homer <oceanuszeng@gmail.com>
2017-07-13 20:07:43 +0800

    build pages structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts
+
+  resources :users
+
+  resources :account
+
+  namespace :admin do
+    resources :users
+    resources :posts
+  end
+
+  root 'posts#index'
 end
