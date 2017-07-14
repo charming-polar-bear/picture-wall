5d5ad3d26e0ca8f1e8ab554456e82362cb108213
homer <oceanuszeng@gmail.com>
2017-07-14 11:11:04 +0800

    user can like post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :like_by_current_user, :dislike_by_current_user]
+  before_action :get_post, only: [:show, :destroy, :current_user_like, :current_user_dislike]
 
   def index
     @posts = Post.all
@@ -21,17 +22,35 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
-
-    # @post.delete
   end
+
+  def current_user_like
+    if !@post.is_liked_by?(current_user)
+      @post.like_by!(current_user)
+    end
+
+    redirect_to post_path(@post)
+  end
+
+  def current_user_dislike
+    if @post.is_liked_by?(current_user)
+      @post.dislike_by!(current_user)
+    end
+
+    redirect_to post_path(@post)
+  end
+
 
   private
     def post_params
       params.require(:post).permit(:picture, :description)
     end
+
+    def get_post
+      @post = Post.find(params[:id])
+    end
+
 end
