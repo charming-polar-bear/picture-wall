9cca8e394b5a2eed13d5e76830046965171dc60a
homer <oceanuszeng@gmail.com>
2017-07-15 21:39:58 +0800

    helper post_deletable? and comment_deletable?

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def comment_deletable?(user)
+    user && user == comment.user || user == comment.post.user || user.is_actived_admin
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def post_deletable?(user)
+    user && user == post.user || user.is_actived_admin
+  end
 end
