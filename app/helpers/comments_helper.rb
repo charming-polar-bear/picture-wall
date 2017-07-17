e40727388f9eccd2f3b55d6eb1365e9e056badb5
homer <oceanuszeng@gmail.com>
2017-07-17 09:43:00 +0800

    fix delete right bug

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,10 @@
 module CommentsHelper
   def comment_deletable?(comment, user)
     # user && user == comment.user || user == comment.post.user || user.is_actived_admin
-    true
+    if user.blank?
+      false
+    else
+      user == comment.user || user == comment.post.user || user.is_actived_admin
+    end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,5 +2,10 @@ module PostsHelper
   def post_deletable?(post, user)
     true
     # user && user == post.user || user.is_actived_admin
+    if user.blank?
+      false
+    else
+      user == post.user || user.is_actived_admin
+    end
   end
 end
